23b691b5ed139c875d550238c3275feda46869d2
MohammadRadja <radja.amri28@gmail.com>
2024-08-06 16:21:53 +0700

    default

--- app.js
+++ app.js
@@ -16,7 +16,8 @@ import dataPegawaiRoute from "./prisma/routes/pegawaiRoute.js";
 dotenv.config();
 
 const app = express();
-const port = process.env.PORT || 3000;
+// const port = process.env.PORT || 3000;
+const port = process.env.PORT;
 
 // Middleware CORS
 const corsOptions = {
